0ccb6c18fab6f790a27e8dfd9eac1606903e8a9c
PNierop <p.c.nierop@student.tue.nl>
2022-10-05 13:29:15 +0200

    Changed get request to use sql database, still need to do the same for the put request

--- flask_api.py
+++ flask_api.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify, abort
 from flask_restful import Api, Resource, reqparse, http_status_message
 import pandas as pd
 import sqlite3
+from hashlib import sha256
+import key
 
-
-conn = sqlite3.connect("Barcodes")
-db = pd.read_pickle("fresh_dataframe.pickle")
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,36 +12,50 @@ api = Api(app)
 args = reqparse.RequestParser()
 args.add_argument("barcode", type=int, help="barcode number")
 args.add_argument("id", type=str)
+args.add_argument("key",type=int, required=True)
 
 
 class ProductInfo(Resource):
     def get(self):
         arg = args.parse_args()
-        try:
-            ##TODO: CHANGE THIS TO SQL QUERY
-            ##TODO: ADD SHA AUTH
-            products = db.loc[db["barcode"]==int(arg["barcode"])][["barcode","id","certainty"]]
-            return {'barcode': arg["barcode"], "products" : products.to_dict('r')}, 200
-        except KeyError as e:
-            abort(400)
+        if True:#sha256(arg["key"]) == key.key:
+            conn = sqlite3.connect("Barcodes.sql")
+            cur = conn.cursor()
 
+            data = cur.execute(f'SELECT * FROM barcodes WHERE barcode = "871039852411"')
+            products = data.fetchall()
+            return_payload = []
+            for prod in products:
+                return_payload.append({"barcode": prod[1], "id": prod[2], "certainty":prod[3]})
+            conn.close()
+            try:
+                return {'barcode': arg["barcode"] , "products":[{"barcode": prod[1], "id": prod[2], "certainty":prod[3]} for prod in products]}, 200
+            except KeyError as e:
+                conn.close()
+                abort(400)
+
+        else:
+            abort(401)
 
     def post(self):
         arg = args.parse_args()
-        ##TODO: CHANGE THIS TO SQL QUERY
-        if arg["barcode"] in db["barcode"]:
-            if arg["id"] in db.loc["barcode","id"]:
-                cert = db.loc["barcode","certainty"]
-                cert[db.loc["barcode", "id"].index(arg["id"])] = cert[db.loc["barcode", "id"].index(arg["id"])] +1
-                db.loc["barcode", "certainty"] = cert
+        if True:#(sha256(arg["key"]) == secrets.key):
+            ##TODO: CHANGE THIS TO SQL QUERY
+            ##TODO: ADD SHA AUTH
+            if arg["barcode"] in db["barcode"]:
+                if arg["id"] in db.loc["barcode","id"]:
+                    cert = db.loc["barcode","certainty"]
+                    cert[db.loc["barcode", "id"].index(arg["id"])] = cert[db.loc["barcode", "id"].index(arg["id"])] +1
+                    db.loc["barcode", "certainty"] = cert
+            else:
+                pd.concat([db,pd.DataFrame({"barcode":[arg["barcode"]],"id":[arg["id"]],"certainty":[1]})])
+            return {"barcode": arg["barcode"], "id":arg["id"]}
         else:
-            pd.concat([db,pd.DataFrame({"barcode":[arg["barcode"]],"id":[arg["id"]],"certainty":[1]})])
-        return {"barcode": arg["barcode"], "id":arg["id"]}
-
+            abort(401)
 
 api.add_resource(ProductInfo, "/api/v1")
 
 if __name__ == "__main__":
-    print(db)
+    #print(db)
     app.run(debug=True)
-    db.to_pickle("fresh_dataframe.pickle")
+    #db.to_pickle("fresh_dataframe.pickle")
--- requests_api.py
+++ requests_api.py
@@ -11,8 +11,8 @@ import pandas as pd
 # db.to_pickle("cleaned_food.pickle")
 
 
-print(req.post("http://127.0.0.1:5000/api/v1", data=json.dumps({"barcode": 26772226, "id":"1"}), headers={'content-type': 'application/json'}).text)
-# print(req.get("http://127.0.0.1:5000/api/v1", data=json.dumps({"barcode": 26772226}), headers={'content-type': 'application/json'}).json())
+# print(req.post("http://127.0.0.1:5000/api/v1", data=json.dumps({"barcode": 26772226, "id":"1"}), headers={'content-type': 'application/json'}).text)
+print(req.get("http://127.0.0.1:5000/api/v1", data=json.dumps({"barcode": 871039852411, "key": 1}), headers={'content-type': 'application/json'}).json())
 #
 # d= {"barcode":[871039852411],"id":["17"],"certainty":[1]}
 # test_db = pd.DataFrame(d)
