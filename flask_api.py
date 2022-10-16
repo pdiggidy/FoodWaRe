a36ef31ef79080401bab007787f88652b5ecd5e3
Philip <p.c.nierop@student.tue.nl>
2022-10-16 16:48:32 +0200

    im going crazy

--- flask_api.py
+++ flask_api.py
@@ -33,7 +33,7 @@ def update_values(old, new, barcode):
     if not first:
         id_dicts = []
         for i in old:
-            id_dicts = json.loads(old)
+            id_dicts = json.loads(i)
         found = False
         for item in id_dicts[0]:
             if item["id"] == new["id"] and item["quantity"] == new["quantity"]:
